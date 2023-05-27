c2e35c445f1599939c067f2e8f7cccca198aa2ea
Pierre Borgmann <info@drweissbrot.net>
2023-05-28 01:53:32 +0200

    #52: serve Quantico font

--- src/server/dependencies.js
+++ src/server/dependencies.js
@@ -1,7 +1,23 @@
-import { sendStaticFile } from './helpers/send-static-file.js'
+import send from 'koa-send'
 
 export const registerDependencyRoutes = (router) => {
 	router.get('/dependencies/vue.js', sendStaticFile('node_modules/vue/dist/vue.esm-browser.js'))
 	router.get('/dependencies/vue3-sfc-loader.js', sendStaticFile('node_modules/vue3-sfc-loader/dist/vue3-sfc-loader.esm.js'))
+
 	router.get('/dependencies/normalize.css', sendStaticFile('node_modules/normalize.css/normalize.css'))
+
+	serveFontsourceFont(router, 'quantico')
+}
+
+// NB! Do _not_ use this with user-supplied values for localFile!
+export const sendStaticFile = (localFile) => async (context) => {
+	await send(context, localFile)
+}
+
+export const serveFontsourceFont = (router, fontName) => {
+	const prefix = `/dependencies/${fontName}/`
+
+	router.get(`${prefix}:path*`, async (context) => {
+		await send(context, context.path.substring(prefix.length), { root: `node_modules/@fontsource/${fontName}` })
+	})
 }
--- src/server/helpers/send-static-file.js
+++
@@ -1,8 +0,0 @@
-import send from 'koa-send'
-
-import { rootDirectory } from './paths.js'
-
-// NB! Do _not_ use this with user-supplied values for localFile!
-export const sendStaticFile = (localFile) => async (context) => {
-	await send(context, localFile, { root: rootDirectory })
-}
