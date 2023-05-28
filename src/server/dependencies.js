7d12bd007d41a7784f5d733914a52b61b4301087
Pierre Borgmann <info@drweissbrot.net>
2023-05-28 06:33:32 +0200

    #52: silence vue3-sfc-loader sourcemap warning

--- src/server/dependencies.js
+++ src/server/dependencies.js
@@ -3,6 +3,7 @@ import send from 'koa-send'
 export const registerDependencyRoutes = (router) => {
 	router.get('/dependencies/vue.js', sendStaticFile('node_modules/vue/dist/vue.esm-browser.js'))
 	router.get('/dependencies/vue3-sfc-loader.js', sendStaticFile('node_modules/vue3-sfc-loader/dist/vue3-sfc-loader.esm.js'))
+	router.get('/dependencies/vue3-sfc-loader.esm.js.map', sendStaticFile('node_modules/vue3-sfc-loader/dist/vue3-sfc-loader.esm.js.map'))
 
 	router.get('/dependencies/normalize.css', sendStaticFile('node_modules/normalize.css/normalize.css'))
 
