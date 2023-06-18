4823f2fa3708830ff061b7fb469715986f17321d
Pierre Borgmann <info@drweissbrot.net>
2023-06-18 17:34:43 +0200

    #52 Fennec: configurable: show round winner panel into freezetime

--- src/themes/fennec/top-bar/team/round-winner/round-winner.js
+++ src/themes/fennec/top-bar/team/round-winner/round-winner.js
@@ -15,9 +15,23 @@ export default {
 		},
 
 		isRoundWinnerPanelActive() {
-			return (this.$round.phase === 'over' && this.$round.winningSide === this.team.side)
-			// TODO configurable
-			// || (this.$round.isFreezetime && this.$rounds[this.$rounds.length - 1].winningSide === this.team.side)
+			if (this.$round.phase === 'over') {
+				return this.$round.winningSide === this.team.side
+			}
+
+			if (
+				this.$round.isFreezetime
+				&& this.$opts['preferences.topBar.team.roundWinnerPanel.showIntoFreezetimeSec']
+				&& this.$rounds[this.$rounds.length - 1].winningSide === this.team.side
+			) {
+				this.$opts['cvars.mp_freezetime'] // 20
+				this.$opts['preferences.topBar.team.roundWinnerPanel.showIntoFreezetimeSec'] // 3
+				this.$round.phaseEndsInSec // 15
+
+				return this.$opts['cvars.mp_freezetime'] - this.$round.phaseEndsInSec < this.$opts['preferences.topBar.team.roundWinnerPanel.showIntoFreezetimeSec']
+			}
+
+			return false
 		},
 	},
 }
