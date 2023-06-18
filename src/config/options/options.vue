24bcccad32f8ef1ab4717352697029002d809495
Pierre Borgmann <info@drweissbrot.net>
2023-06-18 14:34:53 +0200

    #52 Cleanup: split config options css

--- src/config/options/options.vue
+++ src/config/options/options.vue
@@ -1,3 +1,5 @@
 <script src="/config/options/options.js"></script>
-<style src="/config/options/options.css" scoped></style>
+<style src="/config/options/buttons.css" scoped></style>
+<style src="/config/options/option-inputs.css" scoped></style>
+<style src="/config/options/sections.css" scoped></style>
 <template src="/config/options/options.html"></template>
