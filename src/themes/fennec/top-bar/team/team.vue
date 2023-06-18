79ad527e3fe43e346ecb38d77373654f0c892701
Pierre Borgmann <info@drweissbrot.net>
2023-06-18 14:30:24 +0200

    #52 Cleanup: only load shared CSS once

---
+++ src/themes/fennec/top-bar/team/team.vue
@@ -0,0 +1,5 @@
+<script src="/hud/top-bar/team/team.js"></script>
+<style src="/hud/top-bar/team/team.css" scoped></style>
+<template src="/hud/top-bar/team/team.html"></template>
+
+<style src="/hud/top-bar/team/panel-shared.css"></style>
