6eb6866e68fa6eab0b1bdf47da8fa92adca39e4b
Pierre Borgmann <info@drweissbrot.net>
2023-06-18 17:03:27 +0200

    #52 Fennec: Clock red at <= 10 seconds

--- src/themes/fennec/top-bar/center/current-round/clock/clock.js
+++ src/themes/fennec/top-bar/center/current-round/clock/clock.js
@@ -13,5 +13,14 @@ export default {
 		clockSeconds() {
 			return Math.ceil(this.$round.phaseEndsInSec % 60)
 		},
+
+		isClockRed() {
+			if (this.$round.phase === 'freezetime' && ! this.$opts['preferences.topBar.clock.tenSecondsRedInFreezetime']) return false
+			if ((this.$round.phase === 'timeout_ct' || this.$round.phase === 'timeout_t') && ! this.$opts['preferences.topBar.clock.tenSecondsRedInTacticalTimeout']) return false
+			if (this.$round.phase === 'over' && ! this.$opts['preferences.topBar.clock.tenSecondsRedInRoundRestartDelay']) return false
+
+			return this.clockMinutes === 0
+				&& this.clockSeconds <= 10
+		},
 	},
 }
