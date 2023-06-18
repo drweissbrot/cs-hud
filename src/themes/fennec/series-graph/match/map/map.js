24a3813c773baff6a1f3a5839f3cd98138acb1c4
Pierre Borgmann <info@drweissbrot.net>
2023-06-18 15:28:54 +0200

    #52 Cleanup: rename series graph map name component

--- src/themes/fennec/series-graph/match/map/map.js
+++ src/themes/fennec/series-graph/match/map/map.js
@@ -1,4 +1,4 @@
-import Name from '/hud/series-graph/match/map/name/name.vue'
+import MapName from '/hud/series-graph/match/map/map-name/map-name.vue'
 import Pick from '/hud/series-graph/match/map/pick/pick.vue'
 import PickTeam from '/hud/series-graph/match/map/pick-team/pick-team.vue'
 
@@ -8,7 +8,7 @@ export default {
 	],
 
 	components: {
-		Name,
+		MapName,
 		Pick,
 		PickTeam,
 	},
