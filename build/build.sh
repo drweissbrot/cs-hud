19fe970428cfe6692dfc7d15a2db97b032913092
Pierre Borgmann <info@drweissbrot.net>
2023-06-21 02:34:54 +0200

    #60: Electron-based wrappers around HUD, config, radar

--- build/build.sh
+++ build/build.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
-# This script duplicates the csgo-hud repository (by git clone-ing it from the file system), runs a Docker container,
-# builds pre-packaged executables for Windows and Linux (see package.sh for details), and then cleans up after itself.
+# This script...
+#     duplicates the csgo-hud repository (by git clone-ing it from the file system),
+#     builds a Docker image with dependencies for electron-forge,
+#     runs an ephemeral Docker container,
+#     builds pre-packaged executables for Windows and Linux (see package.sh for details),
+#     builds Electron-based wrappers around the HUD, config, and radar web pages,
+#     deletes the Docker image,
+#     zips/tar+gzips the browser wrapper directories,
+#     and then cleans up after itself.
 #
 # The git clone is done to prevent untracked files from ending up in the binaries. I can save my social security number
 # in the project directory, and as long as the file is not committed, it won't be in the binary.
@@ -19,12 +26,40 @@ fi
 mkdir -p tmp/bin
 git clone --no-hardlinks .. tmp/clone
 
+docker build -t drweissbrot/csgo-hud-build .
+
+# build the main executable
 docker run \
+	--rm \
 	-u node \
-	-v ./tmp/clone:/home/node/app \
 	-v ./tmp/bin:/home/node/bin \
+	-v ./tmp/clone:/home/node/app \
 	-w /home/node/app \
-	node:18-alpine \
+	drweissbrot/csgo-hud-build \
 	sh /home/node/app/build/package.sh
 
-rm -r tmp/clone
+docker image rm drweissbrot/csgo-hud-build
+
+cd tmp/bin
+
+# zip browser wrappers for Windows
+zip -r csgo-hud-config-win32-x64.zip csgo-hud-config-win32-x64
+zip -r csgo-hud-radar-win32-x64.zip csgo-hud-radar-win32-x64
+zip -r csgo-hud-win32-x64.zip csgo-hud-win32-x64
+
+# tar and gzip browser wrappers for Linux
+tar -czf csgo-hud-config-linux-x64.tar.gz csgo-hud-config-linux-x64
+tar -czf csgo-hud-linux-x64.tar.gz csgo-hud-linux-x64
+tar -czf csgo-hud-radar-linux-x64.tar.gz csgo-hud-radar-linux-x64
+
+cd ../..
+
+# clean up
+rm -r \
+	tmp/bin/csgo-hud-config-linux-x64 \
+	tmp/bin/csgo-hud-config-win32-x64 \
+	tmp/bin/csgo-hud-linux-x64 \
+	tmp/bin/csgo-hud-radar-linux-x64 \
+	tmp/bin/csgo-hud-radar-win32-x64 \
+	tmp/bin/csgo-hud-win32-x64 \
+	tmp/clone
