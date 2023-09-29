e2e8dd3fcc245508c659fdf6be4d18cb8d0830ee
Pierre Borgmann <info@drweissbrot.net>
2023-09-30 01:54:14 +0200

    fix round graph in MR12 overtimes
    mr12 is still bad dont at me

--- src/themes/raw/gsi/parse-rounds.js
+++ src/themes/raw/gsi/parse-rounds.js
@@ -1,7 +1,7 @@
-import { gsiState, teams } from '/hud/core/state.js'
+import { gsiState, options, teams } from '/hud/core/state.js'
 
 const getPlainReason = (reason) => {
-	// TODO
+	// TODO add anything missing
 	switch (reason) {
 		case 'ct_win_defuse': return 'defuse'
 		case 'ct_win_time': return 'time'
@@ -15,11 +15,13 @@ const getPlainReason = (reason) => {
 
 // NB! This must be called AFTER parseTeams!
 export const parseRounds = () => {
+	const mpMaxrounds = options['cvars.mp_maxrounds']
+	const mpOvertimeMaxrounds = options['cvars.mp_overtime_maxrounds']
+
 	// GSI's map.round_wins resets every overtime, work around that
-	// TODO this may not be based on static 30 and 6, but instead of mp_maxrounds and mp_overtime_maxrounds, need to investigate
-	const roundNumberOffset = gsiState.map.round <= 30
+	const roundNumberOffset = gsiState.map.round <= mpMaxrounds
 		? 0
-		: 30 + Math.floor((gsiState.map.round - 30) / 6) * 6
+		: mpMaxrounds + Math.floor((gsiState.map.round - mpMaxrounds) / mpOvertimeMaxrounds) * mpOvertimeMaxrounds
 
 	return Object.entries(gsiState.map.round_wins || {}).map(([roundNumber, reason]) => {
 		const winningSide = reason.startsWith('ct_win') ? 3 : 2
